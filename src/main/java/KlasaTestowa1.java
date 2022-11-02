484e789cefc34f7eb928aea5d76bf83e9fbf58f6
mskorup660 <117320325+mskorup660@users.noreply.github.com>
2022-11-02 23:27:32 +0100

    1 zapis

---
+++ src/main/java/KlasaTestowa1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KlasaTestowa1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        String name = scanner.nextLine();
+
+
+        System.out.println("podaj imię" + scanner);
+        System.out.println("Czesc" + name);
+    }
+
+}
